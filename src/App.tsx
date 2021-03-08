c9dd93e5af479725f0425030a5a4c59ab41cacd6
ThanhAn <annt@ows.vn>
2021-03-08 09:27:48 +0700

    add mobx store

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
 import Login from "./login";
+import { SessionStore } from "./stores/sessionStore";
 
 class App extends React.Component<any, any> {
   render() {
-    return <Login />;
+    const sessionStore = new SessionStore();
+    return (
+      <>
+        <Login store={sessionStore} />
+      </>
+    )
   }
 }
 

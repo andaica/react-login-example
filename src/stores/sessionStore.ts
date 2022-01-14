d8b4b362716ed29be7dc0ef76b86a3e5f517f719
ThanhAn <annt@ows.vn>
2022-01-14 18:16:22 +0700

    add demo api

--- src/stores/sessionStore.ts
+++ src/stores/sessionStore.ts
@@ -1,4 +1,5 @@
 import { observable, action, computed, makeObservable } from "mobx";
+import { postData } from "../service/api";
 
 export class SessionStore {
   @observable email: string = "";
@@ -9,7 +10,6 @@ export class SessionStore {
   }
 
   @computed get info() {
-    console.log("computed!")
     return `Input data: ${this.email}/${this.password}`;
   }
 
@@ -19,5 +19,17 @@ export class SessionStore {
 
   @action setPassword(password: string): void {
     this.password = password;
+  }
+
+  login = async () => {
+    const response = await postData("https://jako-dev.com/api/user/login", {
+      authenticate_info: this.email,
+      password: this.password,
+      device_uuid: "1234567890111213"
+    })
+    console.log("response: ", response);
+    // save response.data.token
+    // save response.data.user
+    return true;
   }
 }
